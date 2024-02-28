9ecfecdd5330e76dda45dd5ceac4bfe4ff5530ce
JohnLun <jonathan.lun23@gmail.com>
2024-02-27 19:12:24 -0500

    collapsable chat box + style changes

--- src/Components/Transcript.tsx
+++ src/Components/Transcript.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../Styles/Transcript.css";
-
+import Accordion from 'react-bootstrap/Accordion';
 
 function Transcript() {
   // State to manage the visibility of the text div
@@ -12,24 +12,32 @@ function Transcript() {
   };
 
   return (
-    <div className="container rounded-5 chatBox position-absolute bottom-0 start-50 translate-middle">
-      <div className="row p-2">
-        <div className="col"></div>
-        <div className="col col-md-auto">
-          <button className="btn btn-primary chatButton w-100 position-relative top-0 end-0 px-2">
-            Finish Interaction
-          </button>
-        </div>
-        <div className="col col-lg-2">
-          <button className="btn btn-primary chatButton w-100 position-relative top-0 end-0 px-2" onClick={toggleVisibility}>
-            {isVisible ? "Hide Chat" : "Show Chat"}
-          </button>
-        </div>
-      </div>
-      <div className="row">
-          {/* Render the text div conditionally based on the state */}
-          {isVisible && <div>Baguette</div>}
-      </div>
+    <div className="container position-absolute bottom-0 start-50 translate-middle">
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <div className="row p-2">
+            <div className="col"></div>
+            <div className="col col-lg-2">
+               <Accordion.Header>
+                  <button className="btn btn-primary chatButton w-100 position-relative top-0 end-0 px-2" onClick={toggleVisibility}>
+                    {isVisible ? "Hide Chat" : "Show Chat"}
+                  </button>
+                </Accordion.Header>
+            </div>
+            <div className="col col-md-auto">
+              <button className="btn btn-primary chatButton w-100 position-relative top-0 end-0 px-2">
+                Finish Interaction
+              </button>
+            </div>
+            
+          </div>
+          <Accordion.Body>
+            <div>
+              Baguette
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
