38d4711772e5cae5fc10e77cb0e4d6848a80df6c
RishiSajay <sajay.rishi@gmail.com>
2024-03-18 20:09:50 -0400

    Added basic welcome/home page

---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <>
+      <div>Welcome To PenPals!</div>
+      <a href={"/home"}>Click Me!</a>
+    </>
+  );
+};
+
+export default Welcome;
