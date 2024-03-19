4f9ca8498808f6a010fff72e2dbbee8b3e83b965
Katherine Hartley <k.hartley@ufl.edu>
2024-03-18 21:10:06 -0400

    logging in

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,19 +1,5 @@
 import React from "react";
 
-// const textE = document.createElement("input");
-// textE.type = "text";
-// textE.placeholder = "Enter your email";
-// textE.value = "";
-// textE.id = "email";
-// document.getElementsByTagName('body')[0].appendChild(textE);
-
-// const textP = document.createElement("input");
-// textP.type = "password";
-// textP.placeholder = "Enter your password";
-// textP.value = "";
-// textP.id = "password";
-// document.getElementsByTagName('body')[0].appendChild(textP);
-
 const Login = () => {
   return (
     <>
