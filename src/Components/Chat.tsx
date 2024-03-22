d2bc49e2a14f9b0991129881242935465e5738a2
RishiSajay <sajay.rishi@gmail.com>
2024-03-22 17:35:43 -0400

    highlight words

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,42 +1,41 @@
-import Definition from './Definition';
+import Definition from "./Definition";
 import React, { useState } from "react";
+import "../Styles/Chat.css";
 
 interface chatProps {
-    botChat: string[];
-    userChat: string[];
+  botChat: string[];
+  userChat: string[];
 }
 
-function Chat(chats : chatProps) {
-    const [showCard, setShowCard] = useState("");
+function Chat(chats: chatProps) {
+  const [showCard, setShowCard] = useState("");
 
-
-    const handleClick = (word : string) => {
-        setShowCard(word);
-    }
-    return(
-        <div>
-          {chats.botChat.map((sentence, sentenceIndex) => (
-            <div key={sentenceIndex}>
-              {sentence.split(" ").map((word, wordIndex) => (
-                <span key={wordIndex}>
-                  {wordIndex > 0 && " "}
-                  <span onClick={() => handleClick(word)} style={{ cursor: 'pointer' }}>
-                    {word}
-                  </span>
-                </span>
-              ))}
-            </div>
+  const handleClick = (word: string) => {
+    setShowCard(word);
+  };
+  return (
+    <div>
+      {chats.botChat.map((sentence, sentenceIndex) => (
+        <div key={sentenceIndex}>
+          {sentence.split(" ").map((word, wordIndex) => (
+            <span key={wordIndex}>
+              {wordIndex > 0 && " "}
+              <span
+                onClick={() => handleClick(word)}
+                className="word"
+                title={word}
+              >
+                {word}
+              </span>
+            </span>
           ))}
-          <div className="position-fixed top-0 end-0">
-            {showCard != "" &&
-              <Definition word={showCard}>
-                  
-              </Definition>
-            }
-            
-          </div>
         </div>
-    );
+      ))}
+      <div className="position-fixed top-0 end-0">
+        {showCard != "" && <Definition word={showCard}></Definition>}
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
