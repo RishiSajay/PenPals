f7b541ba03d9facadaa349f699b86c97977fb6e1
JohnLun <jonathan.lun23@gmail.com>
2024-03-22 19:46:00 -0400

    added translations

--- src/Components/Definition.tsx
+++ src/Components/Definition.tsx
@@ -7,54 +7,22 @@ import translateText from "../Scripts/GoogleTranslate";
 
 interface definitionProps {
   word: string;
+  trans: string;
 }
 
-/* IMPORTANT: Start of comment for credential issue
 
-const { VITE_REACT_APP_CREDENTIALS } = import.meta.env;
-let CREDENTIALS;
-if (VITE_REACT_APP_CREDENTIALS) {
-  CREDENTIALS = JSON.parse(VITE_REACT_APP_CREDENTIALS);
-}
-
-const API_KEY = CREDENTIALS.private_key;
-const API_URL = "https://translation.googleapis.com/language/translate/v2";
-
-let frenchWord;
-translateText();
-
-End of comment */
 
 function Definition(props: definitionProps) {
-  // const translateText = async (text : string) => {
-  //     fetch(`${API_URL}?` + new URLSearchParams({
-  //         q: "bonjour",
-  //         target: "en",
-  //         format: "text",
-  //         source: "fr",
-  //         model: "base",
-  //         key: API_KEY
-  //     }))
-  //     .then((res) => {
-  //         return res.json();
-  //     })
-  //     .then((data) => {
-  //         console.log(data);
-  //     })
-  // };
-
-  // const frenchWord = translateText(props.word);
-  // console.log(frenchWord);
-
-  return (
+    return (
     <div>
-      <Card>
+        <Card>
         <Card.Body>
-          <Card.Title>{`Word: ${props.word}`}</Card.Title>
+            <Card.Title>{`Word: ${props.word}`}</Card.Title>
+            <Card.Text>{`Translation: ${props.trans}`}</Card.Text>
         </Card.Body>
-      </Card>
+        </Card>
     </div>
-  );
+    );
 }
 
 export default Definition;
