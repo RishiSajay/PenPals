6368de3b46cbaf6d454b6ace867ceff8fb8421eb
RishiSajay <sajay.rishi@gmail.com>
2024-03-22 16:52:36 -0400

    added styles

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -15,48 +15,50 @@ const Login = () => {
         <a href={"/"}>Back</a>
       </body> */}
       <div className="d-flex justify-content-center">
-        <div className="w-25 mt-5">
-          <form>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email Address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-              ></input>
-              <small id="emailHelp" className="form-text text-muted">
-                We'll never share your email with anyone else
-              </small>
-            </div>
-            <div className="form-group mt-2">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              ></input>
-            </div>
-            <div className="form-group mt-2">
-              <label htmlFor="exampleInputPassword1">Confirm Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Confirm Password"
-              ></input>
-            </div>
-            <div className="d-flex mt-2">
-              <a href="/home" className="btn btn-primary me-3">
-                Sign Up
-              </a>
-              <a href="/" className="btn btn-primary">
-                Back
-              </a>
-            </div>
-          </form>
+        <div className="card card w-25 mt-5 border border-primary rounded">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email Address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                ></input>
+                <small id="emailHelp" className="form-text text-muted">
+                  We'll never share your email with anyone else
+                </small>
+              </div>
+              <div className="form-group mt-2">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                ></input>
+              </div>
+              <div className="form-group mt-2">
+                <label htmlFor="exampleInputPassword1">Confirm Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Confirm Password"
+                ></input>
+              </div>
+              <div className="d-flex mt-2">
+                <a href="/home" className="btn btn-primary me-3">
+                  Sign Up
+                </a>
+                <a href="/" className="btn btn-primary">
+                  Back
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
