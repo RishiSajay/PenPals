7ee103ac5294caec26ceb398a0098970cb51b5af
RishiSajay <sajay.rishi@gmail.com>
2024-04-04 11:36:58 -0400

    chat page only reads and writes once now

--- src/Components/Goals.tsx
+++ src/Components/Goals.tsx
@@ -1,14 +1,13 @@
-import { ProgressBar } from "react-bootstrap"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useEffect, useState} from "react"
-import axios, { AxiosResponse } from "axios"
-
+import { ProgressBar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 
 const Goals = () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const user = urlParams.get('user');
-  console.log(user);
-  
+  const user = urlParams.get("user");
+  //console.log(user);
+
   //const user = "1@gmail.com" // hardcoding
   const [XPG, setXPG] = useState("");
   const [XPD, setXPD] = useState("");
@@ -22,38 +21,36 @@ const Goals = () => {
   const [WT, setWT] = useState("");
   const [XP, setXP] = useState("");
   const [H, setH] = useState("");
-  
 
   function getCurrGoals() {
     const task = "read_goals";
     //event.preventDefault();
     axios
-    .post(
-      "https://qeetqm5h08.execute-api.us-east-1.amazonaws.com/prod/resource", 
-      {
-        user,
-        task,
-      }
-    )
-    .then(res => setter(res.data.result))
-    .catch(err => console.log(err));
+      .post(
+        "https://qeetqm5h08.execute-api.us-east-1.amazonaws.com/prod/resource",
+        {
+          user,
+          task,
+        }
+      )
+      .then((res) => setter(res.data.result))
+      .catch((err) => console.log(err));
   }
 
   function setter(res: any) {
-    console.log(res)
-    try{
+    console.log(res);
+    try {
       setWS(res.WS);
       setWT(res.WT);
       setXP(res.XP);
       setH(res.H);
-    }
-    catch{
+    } catch {
       // default values that weren't initially set
       console.log("defaulting progress");
-      setWS('0');
-      setWT('0');
-      setXP('0');
-      setH('0');
+      setWS("0");
+      setWT("0");
+      setXP("0");
+      setH("0");
     }
     setWSG(res.WSG);
     setWSD(res.WSD);
@@ -66,11 +63,12 @@ const Goals = () => {
   }
 
   // need these set from the frontend when user chooses goal to update
-  const[goal, setGoal] = useState('');
-  const[val, setVal] = useState('');
-  const[date, setDate] = useState('');
+  const [goal, setGoal] = useState("");
+  const [val, setVal] = useState("");
+  const [date, setDate] = useState("");
 
-  function goalUpdate(event: { preventDefault: () => void; }) { // update existing goal
+  function goalUpdate(event: { preventDefault: () => void }) {
+    // update existing goal
     event.preventDefault();
     console.log("starting update");
     const task = "write_goals";
@@ -97,7 +95,7 @@ const Goals = () => {
       .then((res) => checkRes(res.data))
       .catch((err) => console.log(err));
   }
- 
+
   function checkRes(res: string) {
     console.log("success");
     window.location.href = "/home?user=" + user;
@@ -107,10 +105,10 @@ const Goals = () => {
     getCurrGoals();
   }, []);
 
-    return (
-      <>                
-         <form onSubmit={goalUpdate}>
-         <div className="d-flex justify-content-center">
+  return (
+    <>
+      <form onSubmit={goalUpdate}>
+        <div className="d-flex justify-content-center">
           <div className="card card w-25 mt-5 border border-primary rounded">
             {" "}
             <div className="card-body">
@@ -136,7 +134,11 @@ const Goals = () => {
               </div> */}
               <div className="form-group mt-2">
                 <label htmlFor="WS">Words Spoken Goal</label>
-                <ProgressBar variant="info" now={Number(WS)} max={Number(WSG)}></ProgressBar>
+                <ProgressBar
+                  variant="info"
+                  now={Number(WS)}
+                  max={Number(WSG)}
+                ></ProgressBar>
                 <input
                   type="int"
                   className="form-control"
@@ -156,7 +158,11 @@ const Goals = () => {
               </div>
               <div className="form-group mt-3">
                 <label htmlFor="WT">Words Typed Goal</label>
-                <ProgressBar variant="info" now={Number(WT)} max={Number(WTG)}></ProgressBar>
+                <ProgressBar
+                  variant="info"
+                  now={Number(WT)}
+                  max={Number(WTG)}
+                ></ProgressBar>
                 <input
                   type="int"
                   className="form-control"
@@ -176,7 +182,11 @@ const Goals = () => {
               </div>
               <div className="form-group mt-4">
                 <label htmlFor="H">Highlighting Goal</label>
-                <ProgressBar variant="info" now={Number(H)} max={Number(HG)}></ProgressBar>
+                <ProgressBar
+                  variant="info"
+                  now={Number(H)}
+                  max={Number(HG)}
+                ></ProgressBar>
                 <input
                   type="int"
                   className="form-control"
@@ -200,12 +210,9 @@ const Goals = () => {
             </div>
           </div>
         </div>
+      </form>
+    </>
+  );
+};
 
-          </form>
-
-      </>
-    );
-  };
-  
-  export default Goals;
-  
+export default Goals;
